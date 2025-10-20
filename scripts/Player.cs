a9d3dd3466dd498361a35c900a0827c49d94abfa
zanadoman <domanseven@gmail.com>
2025-10-20 21:40:10 +0200

    idle anim

--- scripts/Player.cs
+++ scripts/Player.cs
@@ -1,11 +1,15 @@
 using Godot;
 using System;
 
-public partial class PlayerTexture : Sprite2D
+public partial class Player : Node2D
 {
+	private AnimatedSprite2D _sprite;
+	
 	// Called when the node enters the scene tree for the first time.
 	public override void _Ready()
 	{
+		_sprite = GetNode<AnimatedSprite2D>("Sprite");
+		_sprite.Play("idle");
 	}
 
 	// Called every frame. 'delta' is the elapsed time since the previous frame.

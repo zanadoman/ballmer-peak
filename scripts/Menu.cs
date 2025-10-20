ed372d7866d437785f9f4092a6e75423a853b0eb
Gunics Roland <grolcsi09@gmail.com>
2025-10-20 22:52:15 +0200

    main menu

---
+++ scripts/Menu.cs
@@ -0,0 +1,16 @@
+using Godot;
+using System;
+
+public partial class Menu : Control
+{
+	// Called when the node enters the scene tree for the first time.
+	public override void _Ready()
+	{
+		GetNode<Button>("Button").Pressed += OnStartButtonPressed;
+	}
+
+	private void OnStartButtonPressed()
+	{
+		GetTree().ChangeSceneToFile("res://scenes/World.tscn");
+	}
+}

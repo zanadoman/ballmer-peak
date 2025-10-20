018a14a86806cc79408cd7184f724024cf996e73
zanadoman <domanseven@gmail.com>
2025-10-20 21:58:47 +0200

    player movement

--- scripts/Player.cs
+++ scripts/Player.cs
@@ -1,19 +1,52 @@
 using Godot;
 using System;
 
-public partial class Player : Node2D
+public partial class Player : CharacterBody2D
 {
+	[Export]
+	public float Speed { get; set; } = 50000;
+	
+	[Export]
+	public float Gravity { get; set; } = 1000;
+		
+	[Export]
+	public float JumpStrength { get; set; } = 1000;
+	
 	private AnimatedSprite2D _sprite;
 	
-	// Called when the node enters the scene tree for the first time.
 	public override void _Ready()
 	{
 		_sprite = GetNode<AnimatedSprite2D>("Sprite");
 		_sprite.Play("idle");
 	}
 
-	// Called every frame. 'delta' is the elapsed time since the previous frame.
 	public override void _Process(double delta)
 	{
+		var velocity = Velocity;
+		
+		if (Input.IsActionPressed("Left") && !Input.IsActionPressed("Right"))
+		{
+			velocity.X = -Speed * (float)delta;
+		}
+		else if (Input.IsActionPressed("Right") && !Input.IsActionPressed("Left"))
+		{
+			velocity.X = Speed * (float)delta;
+		}
+		else
+		{
+			velocity.X = 0;
+		}
+		
+		if (!IsOnFloor())
+		{
+			velocity.Y += Gravity * (float)delta;
+		}
+		else if (Input.IsActionJustPressed("Jump"))
+		{
+			velocity.Y = -JumpStrength;
+		}
+		
+		Velocity = velocity;
+		MoveAndSlide();
 	}
 }
